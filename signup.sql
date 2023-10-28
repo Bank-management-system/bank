0c4b9cbc2bbc91baa355623a9f03668c3571e8f9
gaurav1228-code <141988011+gaurav1228-code@users.noreply.github.com>
2023-10-28 13:49:42 +0530

    Delete signup.sql

--- signup.sql
+++
@@ -1,6 +0,0 @@
-create database bankmanagementsystem;
-
-use bankmanagementsystem;
-drop table signup;
-create table signup(formno varchar(20),name varchar(20),fname varchar(20),dob varchar(20),gender varchar(20),email varchar(40),marital varchar(20),address varchar(50),city varchar(30),pincode varchar(20),state varchar(30));
-select * from signup;
